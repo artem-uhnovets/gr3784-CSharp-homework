a4d71b12804f38767eb4581842aec08e1398fdd3
ArtemYU <uhnovets@gmail.com>
2023-01-18 03:42:50 +0300

    create sem2-hw and done task1

---
+++ sem2-hw/task1/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+int a = 456;
+int b = 782;
+int c = 918;
+int resultA = (a/10)%10;
+Console.WriteLine($"У 3-х значного числа {a} 2-ой цифрой является {resultA}");
+int resultB = (b/10)%10;
+Console.WriteLine($"У 3-х значного числа {b} 2-ой цифрой является {resultB}");
+int resultC = (c/10)%10;
+Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой является {resultC}");
+
+// Console.Clear();
+// int a = new Random().Next(100,1000);
+// int b = new Random().Next(100,1000);
+// int c = new Random().Next(100,1000);
+// int resultA = (a/10)%10;
+// Console.WriteLine($"У 3-х значного числа {a} 2-ой цифрой является {resultA}");
+// int resultB = (b/10)%10;
+// Console.WriteLine($"У 3-х значного числа {b} 2-ой цифрой является {resultB}");
+// int resultC = (c/10)%10;
+// Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой является {resultC}");
+
+// Console.Clear();
+// string a;
+// string b;
+// string c;
+
+// do {
+//     Console.Write("Введите 1-ое число ");
+//     a = Console.ReadLine();
+//     if (a.Length != 3) 
+//     {
+//         Console.WriteLine("Введите пожалуйста именно ТРЕХ значное число!");
+//     }
+// } while(a.Length != 3);
+
+// do {
+//     Console.Write("Введите 2-ое число ");
+//     b = Console.ReadLine();
+//     if (b.Length != 3) 
+//     {
+//         Console.WriteLine("Введите пожалуйста именно ТРЕХ значное число!");
+//     }
+// } while(b.Length != 3);
+
+// do {
+//     Console.Write("Введите 3-ье число ");
+//     c = Console.ReadLine();
+//     if (c.Length != 3) 
+//     {
+//         Console.WriteLine("Введите пожалуйста именно ТРЕХ значное число!");
+//     }
+// } while(c.Length != 3);
+
+// int resultA = (int.Parse(a)/10)%10;
+// Console.WriteLine($"У 3-х значного числа {a} 2-ой цифрой является {resultA}");
+// int resultB = (int.Parse(b)/10)%10;
+// Console.WriteLine($"У 3-х значного числа {b} 2-ой цифрой является {resultB}");
+// int resultC = (int.Parse(c)/10)%10;
+// Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой является {resultC}");
