8ad8317911a2a2379a7985b6cfe4a62b9dc47caa
ArtemYU <uhnovets@gmail.com>
2023-01-18 12:45:17 +0300

    add new to task1 at sem2-hw

--- sem2-hw/task1/Program.cs
+++ sem2-hw/task1/Program.cs
@@ -25,6 +25,7 @@ Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой 
 // int resultC = (c/10)%10;
 // Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой является {resultC}");
 
+
 // Console.Clear();
 // string a;
 // string b;
@@ -62,4 +63,44 @@ Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой 
 // int resultB = (int.Parse(b)/10)%10;
 // Console.WriteLine($"У 3-х значного числа {b} 2-ой цифрой является {resultB}");
 // int resultC = (int.Parse(c)/10)%10;
+// Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой является {resultC}");
+
+
+// Console.Clear();
+// int a;
+// int b;
+// int c;
+
+// do {
+//     Console.Write("Введите 1-ое число ");
+//     a = int.Parse(Console.ReadLine());
+//     if (a<100 || a>999) 
+//     {
+//         Console.WriteLine("Введите пожалуйста именно ТРЕХ значное число!");
+//     }
+// } while(a<100 || a>999);
+
+// do {
+//     Console.Write("Введите 2-ое число ");
+//     b = int.Parse(Console.ReadLine());
+//     if (b<100 || b>999) 
+//     {
+//         Console.WriteLine("Введите пожалуйста именно ТРЕХ значное число!");
+//     }
+// } while(b<100 || b>999);
+
+// do {
+//     Console.Write("Введите 3-ье число ");
+//     c = int.Parse(Console.ReadLine());
+//     if (c<100 || c>999) 
+//     {
+//         Console.WriteLine("Введите пожалуйста именно ТРЕХ значное число!");
+//     }
+// } while(c<100 || c>999);
+
+// int resultA = (a/10)%10;
+// Console.WriteLine($"У 3-х значного числа {a} 2-ой цифрой является {resultA}");
+// int resultB = (b/10)%10;
+// Console.WriteLine($"У 3-х значного числа {b} 2-ой цифрой является {resultB}");
+// int resultC = (c/10)%10;
 // Console.WriteLine($"У 3-х значного числа {c} 2-ой цифрой является {resultC}");
