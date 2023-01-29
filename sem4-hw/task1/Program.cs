9db4ca47693e6210e2121feede8c5cd947e45f9b
ArtemYU <uhnovets@gmail.com>
2023-01-29 15:29:39 +0300

    add and complete task1 task2 of sem4-hw

---
+++ sem4-hw/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int PowerNumber(int number, int power)
+{
+    int result = number;
+    if (power == 1) result = number;
+    else for (int count = 2; count <= power; count++)
+        {
+            result = result * number;
+        }
+    return result;
+}
+
+int number = GetNumber("Введите число для возведения в степень:");
+int power = GetNumber("Введите степень:");
+int result = PowerNumber(number, power);
+Console.WriteLine($"{number}, {power} -> {result}");
---
+++ sem4-hw/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int SummDigit(int number)
+{
+    int summ = 0;
+    int tempnumber = number;
+    for (int index = 0; index < number.ToString().Length; index++)
+    {
+        summ = summ + tempnumber%10;
+        tempnumber = tempnumber/10;
+    }
+    return summ;
+}
+
+// int SummDigit(string number)
+// {
+//     int summ = 0;
+//     int[] emptyarr = new int[number.Length];
+//     for (int index = 0; index < number.Length; index++)
+//     {
+//         emptyarr[index] = int.Parse(number[index]);
+//         summ = summ + emptyarr[index];
+//         Console.WriteLine(number[index]);
+//         Console.WriteLine(summ);
+//     }
+//     return summ;
+// }
+
+int number = GetNumber("Введите число:");
+int summ = SummDigit(number);
+Console.WriteLine($"{number} -> {summ}");
