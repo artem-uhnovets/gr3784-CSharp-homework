0d2a15f8471a99ed4de322cbaf1ce7169a909b56
ArtemYU <uhnovets@gmail.com>
2023-01-30 02:32:56 +0300

    add and complete task3 of sem4-hw

---
+++ sem4-hw/task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// пользователь сам задает размер массива
+
+Console.Clear();
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}, ");
+        else if (i == (array.Length - 1)) Console.WriteLine($"{array[i]}]");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int index = 0; index < size; index++)
+    {
+        arr[index] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+int GetSize(string text)
+{
+    Console.WriteLine(text);
+    int size = int.Parse(Console.ReadLine());
+    return size;
+}
+
+int size = GetSize("Введите размер массива:");
+int[] array = GetArray(size, 0, 101);
+Console.Write(String.Join(", ", array));
+Console.Write(" -> ");
+PrintArray(array);
