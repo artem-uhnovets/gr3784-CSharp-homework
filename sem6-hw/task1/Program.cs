a588eac663db6de1488870cce2714ee81ea9fd5f
ArtemYU <uhnovets@gmail.com>
2023-01-31 21:27:23 +0300

    add and complete task1 & 2 of sem6

---
+++ sem6-hw/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int CheckPstv(int[] array)
+{
+    int count = 0;
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] > 0) count++;
+    }
+    return count;
+}
+
+int[] GetArray(int quantity)
+{
+    int[] array = new int[quantity];
+    for (int index = 0; index < quantity; index++)
+    {
+        Console.Write($"Enter {index + 1} value: ");
+        array[index] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+int GetQntity(string text)
+{
+    Console.WriteLine(text);
+    int quantity = int.Parse(Console.ReadLine());
+    return quantity;
+}
+
+int quantity = GetQntity("How many numbers do you want to set?");
+int[] array = GetArray(quantity);
+int count = CheckPstv(array);
+Console.Write(String.Join(", ", array));
+Console.WriteLine($" -> {count}");
---
+++ sem6-hw/task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43: Напишите программу,
+// которая найдёт точку пересечения двух прямых,
+// заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// y - y = k1 * x + b1 - k2 * x - b2
+// 0 = k1 * x + b1 - k2 * x - b2
+// k1 * x - k2 * x = b2 - b1
+// x * (k1 - k2) = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+void CrossLines(float b1, float k1, float b2, float k2)
+{
+    float numX = (b2 - b1) / (k1 - k2);
+    float numY1 = k1 * numX + b1;
+    float numY2 = k2 * numX + b2;
+    if (numY1 == numY2)
+    {
+        Console.WriteLine($"({numX}; {numY1})");
+    }
+    else Console.WriteLine("Lines don't cross.");
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+float numberB1 = GetNum("Введите число B1 - ");
+float numberK1 = GetNum("Введите число K1 - ");
+float numberB2 = GetNum("Введите число B2 - ");
+float numberK2 = GetNum("Введите число K2 - ");
+Console.Write($"b1 = {numberB1}, k1 = {numberK1}, b2 = {numberB2}, k2 = {numberK2} -> ");
+CrossLines(numberB1, numberK1, numberB2, numberK2);
