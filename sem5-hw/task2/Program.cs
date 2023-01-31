2b0ee317bbd55f46e4df97c3fd606ee0581e62e9
ArtemYU <uhnovets@gmail.com>
2023-01-31 12:26:21 +0300

    add and complete tasks sem5

---
+++ sem5-hw/task2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int OddSumm(int[] array)
+{
+    int result = 0;
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (index % 2 != 0)
+        {
+            result = result + array[index];
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}, ");
+        else if (i == (array.Length - 1)) Console.Write($"{array[i]}]");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int index = 0; index < size; index++)
+    {
+        arr[index] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+int GetSize(string text)
+{
+    Console.WriteLine(text);
+    int size = int.Parse(Console.ReadLine());
+    return size;
+}
+
+int size = GetSize("Введите размер массива:");
+int[] array = GetArray(size, -999, 999);
+PrintArray(array);
+int result = OddSumm(array);
+Console.Write($" -> {result}");
---
+++ sem5-hw/task3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу
+// между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+int SummMinMax(int[] array)
+{
+    int result = 0;
+    int min = array[0];
+    int max = array[0];
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] < min)
+        {
+            min = array[index];
+        }
+        if (array[index] > max)
+        {
+            max = array[index];
+        }
+    }
+    result = max - min;
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}, ");
+        else if (i == (array.Length - 1)) Console.Write($"{array[i]}]");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int index = 0; index < size; index++)
+    {
+        arr[index] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+int GetSize(string text)
+{
+    Console.WriteLine(text);
+    int size = int.Parse(Console.ReadLine());
+    return size;
+}
+
+int size = GetSize("Введите размер массива:");
+int[] array = GetArray(size, 0, 100);
+PrintArray(array);
+int result = SummMinMax(array);
+Console.Write($" -> {result}");
